087709586db720d6d4de74d9453c25ab025bcb15
Lucia Brammer <brammerl96@gmail.com>
2025-05-07 15:29:06 -0700

    chore: Add click functionality to a lot of things lol

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -9,19 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
             return response.json()
-
         })
         .then((response) => {
             const playlistCards = document.querySelector('.playlist-cards');
 
+            const playlists = response.playlists
 
-            response.playlists.forEach((playlist) => {
+            playlists.forEach((playlist) => {
                 const {playlist_name, playlist_creator, playlist_art, likeCount, playlistID}  = playlist;
 
                 const playlistCard = document.createElement('div')
 
                 playlistCard.classList.add('playlist-card')
-                playlistCard.setAttribute('playlist-id', playlistID.toString());
+                playlistCard.setAttribute('data-playlist-id', playlistID.toString());
 
                 playlistCard.innerHTML = `
                 <img src=${playlist_art}/>
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <p>Created by: ${playlist_creator}</p>
                 </div>
                 <div class="favorite">
-                    <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
+                    <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
                     class="heart">
                         <rect width="24" height="24" fill="none"/>
                         <path d="M21 8.99998C21 12.7539 15.7156 17.9757 12.5857 20.5327C12.2416 20.8137 11.7516 20.8225 11.399 20.5523C8.26723 18.1523 3 13.1225 3 8.99998C3 2.00001 12 2.00002 12 8C12 2.00001 21 1.99999 21 8.99998Z" stroke="#000000" stroke-linecap="round" stroke-linejoin="round"/>
@@ -39,11 +39,12 @@ document.addEventListener('DOMContentLoaded', () => {
                     <p> ${likeCount} </p>
                 `
                 playlistCards.appendChild(playlistCard)
+                addLikeEventListener(playlistID)
             })
-        })
-        .then(() => {
-            addExitClickEvent()
-            addClickEvent()
+
+            addClickEvent(playlists)
+
+            
         })
         .catch((error) => {
 
@@ -53,26 +54,136 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 // Click event function for playlist tiles
-const addClickEvent = () => {
+const addClickEvent = (playlists) => {
     const cards = document.querySelectorAll('.playlist-card')
-    console.log(cards)
 
     cards.forEach((card) => {
-        card.addEventListener('click', () => {
+        const playlistID = card.dataset.playlistID
+
+        card.addEventListener('click', (e) => {
+            const heart =  document.querySelector(`[data-playlist-id = "${playlistID}"] .heart path`)
+            console.log(e.target);
             const overlay = document.querySelector('.modal-overlay')
-            overlay.classList.add('active')
-            document.body.style.overflow = 'hidden';
+            const playlist = playlists.filter((playlist) => playlist.playlistID == playlistID)
+            populateModal(playlist)
+            if(e.target != heart) {
+                overlay.classList.add('active')
+                document.body.style.overflow = 'hidden';
+            }
+
         })
     })
 }
 
-const addExitClickEvent = () => {
-    const exitModalButton = document.querySelector('.exit-button')
+const populateModal = (playlist) => {
+    const {playlist_name, playlist_creator, playlist_art, likeCount, songs} = playlist[0]
 
-    exitModalButton.addEventListener('click', () => {
-        const overlay = document.querySelector('.modal-overlay')
-        overlay.classList.remove('active')
+    const header = document.querySelector('.modal-header')
+
+    header.innerHTML = `
+        <img src=${playlist_art}/>
+        <div class="playlist-info">
+            <h2>${playlist_name}</h2>
+            <h3>Created by: ${playlist_creator}</h3>
+        </div>
+        <div class="exit-button-container">
+            <button class="exit-button">X</button>
+        </div>
+    `
+
+    populateSongList(songs)
+    addExitClickEvent()
+}
+
+const addLikeEventListener = (playlistID) => {
+    const heart = document.querySelector(`[data-playlist-id = "${playlistID}"] .heart`)
+
+    heart.addEventListener('click', () => {
+        const likeCount =  document.querySelector(`[data-playlist-id = "${playlistID}"] .favorite p`)
+
+        if (heart.classList.contains('clicked') && likeCount.innerHTML > 0 ) { 
+            heart.classList.remove('clicked')
+            likeCount.innerHTML = `
+                ${parseInt(likeCount.innerHTML) - 1} 
+            `
+            return;
+        }
+        heart.classList.add('clicked')
+        likeCount.innerHTML = `
+        ${parseInt(likeCount.innerHTML) + 1} 
+    `
+    return;
+
+
+
+           
+        
+        
+    })
+
+}
+
+const populateSongList = (songs) => {
+    songListContainer = document.querySelector('.song-list')
+
+
+    songs.forEach((song) => {
+        const {title, artist, album, duration, songID, cover_art} = song
+
+
+        const songCard = document.createElement('div')
+        songCard.classList.add('song-card')
+        songCard.innerHTML = `
+            <img src=${cover_art}/>
+            <div class="song-info">
+                <ul>
+                    <li><h4>${title}</h4></li>
+                    <li>
+                        <p>${artist}</p>
+                    </li>
+                    <li>
+                        <p>${album}</p>
+                    </li>
+                </ul>
+
+            </div>
+            <div class="time">
+                <p>${duration}</p>
+            </div>
+        `
+
+        songListContainer.appendChild(songCard)
+    })
+
+    addClickEvent()
+}
+
+
+const addExitClickEvent = () => {
+    const overlay = document.querySelector('.modal-overlay')
+
+    overlay.addEventListener('click', (e) => {
+        if(e.target == overlay) {
+            overlay.classList.remove('active')
+        }
     })
 }
+
+const populateHeader = () => {
+    const header = document.querySelector('.modal-header')
+
+    header.innerHTML = `
+        <img src="./assets/img/playlist.png"/>
+        <div class="playlist-info">
+            <h2>Playlist Title</h2>
+            <h3>Creator Name</h3>
+        </div>
+        <div class="exit-button-container">
+            <button class="exit-button">X</button>
+        </div>
+    `
+}
+
+
 
 
